6011cc3ddb2c508374699fae50dcbbd1434b726b
m5fisker <sooryaku1@live.co.uk>
2017-12-03 07:09:42 +0100

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="sqldatacode.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="login_box" runat="server"></asp:TextBox>
+        Login ID<br />
+        <asp:TextBox ID="pwd_box" runat="server"></asp:TextBox>
+        Password<br />
+        <asp:Button ID="Login" runat="server" OnClick="Login_Click" Text="Login" />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:TextBox ID="Box_1" runat="server"></asp:TextBox>
+        <asp:Label ID="Label2" runat="server" Text="Login ID"></asp:Label>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <p>
+            <asp:Button ID="serach_button" runat="server" OnClick="search_button_Click" Text="Search" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace sqldatacode
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=ITLAB01\SQLEXPRESS;Initial Catalog=cse_user;User ID=sa;Password=system123#");
+            con.Open();
+            SqlCommand con1 = new SqlCommand("select * from cseuser where login='" + login_box.Text + "' and pwd='" + pwd_box.Text + "' ", con);
+            SqlDataReader r;
+            r = con1.ExecuteReader();
+            /*GridView1.DataSource = r;
+            GridView1.DataBind();*/
+
+            if (r.HasRows)
+                Label1.Text = "login details valid";
+            else
+                Label1.Text = "login details invalid";
+        }
+
+        protected void search_button_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=ITLAB01\SQLEXPRESS;Initial Catalog=cse_user;User ID=sa;Password=system123#");
+            con.Open();
+            SqlCommand con1 = new SqlCommand("select * from cseuser where login='" + Box_1.Text + "'", con);
+            SqlDataReader r;
+            r = con1.ExecuteReader();
+            GridView1.DataSource = r;
+            GridView1.DataBind();
+        }
+    }
+}
